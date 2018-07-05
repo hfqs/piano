be8f87b627ac919d26859077ce5667a116e3eb86
fsyb <17610000122@163.com>
2018-07-05 10:02:10 +0800

    piano question

--- js/examin.js
+++ js/examin.js
@@ -2,6 +2,21 @@ function check (num){
   var len=$('.answers>div').length
 
   for (var i = 0; i < len; i++) {
-    array[i]
+    if (num==i) {
+      $(`.choice:eq(${i}) img`).addClass('right-answer')
+      $(`.choice:eq(${i})`).append('<img class="checkImg" src="exam-images/正确.png" />')
+
+    }else{
+      $(`.choice:eq(${i}) img`).addClass('wrong-answer')
+        $(`.choice:eq(${i})`).append('<img class="checkImg" src="exam-images/错误.png" />')
+    }
   }
+  $('.checkImg').hide()
 }
+$(".choice").click(function(event) {
+  /* Act on the event */
+  console.log($(this).find('.checkImg'));
+  $(this).find('.checkImg').show()
+});
+
+// alert(1)
